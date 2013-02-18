f77e5b9812332a9e97cb6d01f5ad39f5120a4d63
Ben Yee <l3enny@gmail.com>
2013-02-18 11:46:06 -0500

    New solver script, renamed inout.

--- script.py
+++ script.py
@@ -20,23 +20,10 @@ import inout                # Input/output handling
 import rate                 # Determines reaction rates
 import solvers              # Handles general state calculations
 
-#  Physical system options (user-defined)
-Te = 1.0 * q                         # effective electron temperature, J
-dist = distributions.drumax(0.5, Te) # load EEDF
-ne = 1e10 * 1e6                      # background electron density, 1/m^3
+# User-specified options
+from settings import *               # load user settings file
 from gases import helium as gas      # choose gas to simulate
-Tg = 300                             # neutral gas temperature, K
-P = 10.0 * 133.322                   # neutral gas pressure, Pa
 
-# Solver options (user-defined)
-T = 0.3e-6          # duration to simulate, s
-hmin = 1e-18        # minimum time step, s
-hmax = 1e-09        # maximum time step, s
-TOL = 1.0e-06       # absolute allowable truncation error, 1/m^3
-
-# Output options (user-defined)
-dumprate = 0        # write data to disk after this many steps, 0 = never
-prefix = 'dump'     # file prefix for data files
 
 # You probably don't want to change anything past this line ...
 # ------------------------------------------------------------------------------
