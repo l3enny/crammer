Runs without equilibrium calculation, need to fix for over-determined system.

script.py
@@ -51,19 +51,21 @@ wavefile = open(prefix + '_wavelengths.csv', 'w')
 np.savetxt(wavefile, wavelengths, delimiter=',')
 wavefile.close()
 
-# Solve for equilibrium
-eqerr = 1.0
+# Set the initial conditions
+times = [0.0]
+emissions = [np.zeros(sum(range(1, len(order))))]
+populations = solvers.ion_equilibrium(dfdt, ne, Ng)
+
+
+#TODO: Te and ne lead to an over-determined system. Fix this problem!
 n = ne
-while eqerr > TOL:
+eqerr = 1.0
+while eqerr > TOL:      # Iteratively solve for the equilibrium for the givens
     N = solvers.equilibrium(dfdt(0.0)) * Ng
     eqerr = abs(n - N[-1]) / N[-1]
     n = N[-1]
-
-# Initialize arrays with appropriate starting values
 populations = [N]
-times = [0.0]
-errors = [0.0]
-emissions = [np.zeros(sum(range(1, len(order))))]
+
 
 # Function to append emissions values for each time step
 def rad(A, N, dt):
@@ -74,8 +76,10 @@ def rad(A, N, dt):
     return emits
     
 # Initialize solver and evolve states over time
-Arad = np.fill_diagonal(Ao.copy(), 0)
-stepper = solvers.rkf45(f, times[0], populations[0], hmax, hmin, TOL)
+Arad = Ao.copy()
+np.fill_diagonal(Arad, 0)
+errors = [0.0]
+stepper = solvers.rkf45(dfdt, times[0], populations[0], hmax, hmin, TOL)
 start = datetime.now()
 while times[-1] < T:
     N, t, eps = stepper.next()  # Step to next value with generator function
@@ -94,4 +98,4 @@ while times[-1] < T:
         print "Simulation Time:", (end - start), "\n"
 
 populations = np.array(populations)
-handler.save(times, populations, errors, emissions)
+handler.save(prefix, [times, populations, errors, emissions])

solvers.py
@@ -4,7 +4,7 @@ Sets up the matrix and calculates the solutions.
 
 import numpy as np
 
-def equilibrium(matrix):
+def svd(matrix):
     """
     Determines the solution for a homogeneous system of equations in
     the for Ax = 0. The user supplies a matrix, A, as the input and
@@ -15,6 +15,14 @@ def equilibrium(matrix):
     #TODO: Is the last row of v always associated with the lowest s?
     null = np.abs(v[-1,:]).T
     return null
+
+def ion_equilibrium(dfdt, Ng):
+    err = 1.0
+    while err > TOL:
+        N = equilibrium(dfdt(0.0)) * Ng
+        err = abs(n - N[-1]) / N[-1]
+        n = N[-1]
+    return N
 
 def rkf45(dfdt, t0, y0, hmax, hmin, TOL):
     """