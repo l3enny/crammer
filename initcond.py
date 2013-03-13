3a3702613259cb55584484b70ce03052633c1387
Ben Yee <l3enny@gmail.com>
2013-03-13 18:42:05 -0400

    Added the initial conditions helper module.

---
+++ initcond.py
@@ -0,0 +1,16 @@
+import solvers
+
+def equilibrium(A):
+    # Determines the equilibrium values for the populations based solely on
+    # temperature. NB unless the electron density is subsequently set equal to
+    # the ion density, this will likely result in deviation from neutrality.
+    # Values are normalized to the gas density.
+    err = 1.0
+    TOL = 1.0e-6
+    n = 0.0
+    while err > TOL:
+        N = solvers.svd(A)
+        err = abs(n - N[-1]) / N[-1]
+        n = N[-1]
+    return N
+
