67d815854a9a796c69add0c92f636fad3223b356
Ben Yee <l3enny@gmail.com>
2013-03-14 12:28:31 -0400

    Initial transcription of Kushner rate coefficients. Dropping the importer for the moment.

--- handler.py
+++ handler.py
@@ -15,14 +15,3 @@ def save(data, prefix='dump'):
         with open(prefix + '_' + names[i] + '.csv', 'w') as fid:
             N.savetxt(fid, data[i], delimiter=',')
     return None
-
-def loadkushner(path):
-    # This is a really hacky way of parsing Kushner's rate coefficient file. It
-    # is fragile and does not like change.
-    E = []
-    with open(path) as fid:
-        for line in fid.readlines():
-            # Check if the line marks a new case
-            if '*****' in line:
-                
-        pass
