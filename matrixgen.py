Fixed error in dE calculation.

matrixgen.py
@@ -11,17 +11,6 @@ import numpy as N
 
 import rate
 
-def electronic(gas, Te):
-    states = gas.states.states
-    order = sorted(states.keys(), key=lambda state:states[state]['E'])
-    dim = len(states)
-    mat = N.zeros((dim, dim))
-    for i in range(dim):
-        for f in range(i+1, dim):
-            mat[i, f], mat[f, i] = gas.electronic.rates(Te, order[i], order[f])
-        mat[i, i] = -np.sum(mat[i, :])
-    return mat
-
 def km(gas, Te):
     states = gas.states.states
     order = sorted(states.keys(), key=lambda state:states[state]['E'])
@@ -31,25 +20,16 @@ def km(gas, Te):
         km.append(gas.electronic.rates(Te, order[i], 'elastic')[0])
     return sum(km)
 
-#TODO: If this mode is reimplemented move the super-elastic code to
-#      either the cross section tables, or to a helper file
-#def electronic(gas, f, Te):
-#    states = gas.states.states
-#    order = sorted(states.keys(), key=lambda state:states[state]['E'])
-#    dim = len(states)
-#    mat = N.zeros((dim, dim))
-#    for i in range(dim):
-#        gi = states[order[i]]['g']
-#        Ei = states[order[i]]['E']
-#        for j in range(i + 1, dim):
-#            gf = states[order[j]]['g']
-#            Ef = states[order[j]]['E']
-#            transition = gas.electronic.Transition(order[i], order[j])
-#            down = rate.rate(transition, f)
-#            mat[j, i] = down
-#            mat[i, j] = down * (gf/gi) * N.exp((Ei - Ef)/Te)
-#        mat[i, i] = -sum(mat[:,i])
-#    return mat
+def electronic(gas, Te):
+    states = gas.states.states
+    order = sorted(states.keys(), key=lambda state:states[state]['E'])
+    dim = len(states)
+    mat = N.zeros((dim, dim))
+    for i in range(dim):
+        for f in range(i+1, dim):
+            mat[i, f], mat[f, i] = gas.electronic.rates(Te, order[i], order[f])
+        mat[i, i] = -N.sum(mat[i, :])
+    return mat
 
 def optical(gas):
     states = gas.states.states

script.py
@@ -21,7 +21,7 @@ import matrixgen            # Generates the rate matrices
 import solvers              # Handles general state calculations
 
 # User-specified options
-from settings.kushner import *       # load user settings file
+from settings.sandia import *       # load user settings file
 from gases import helium as gas      # choose gas to simulate
 
 
@@ -37,6 +37,7 @@ order = sorted(states.keys(), key=lambda state:states[state]['E'])
 # Set the initial conditions
 # TODO: Make this a user setting
 Ao = matrixgen.optical(gas)
+print "Ao:", Ao
 Ae = matrixgen.electronic(gas, Te)      # Generate electron rates
 km = matrixgen.km(gas, Te)              # Generate momentum transfer
 def dNdt(t, N):
@@ -54,43 +55,49 @@ def dTedt(t, Te):
     elastic = - ne * (2 * me / M) * km * Ng * 1.5 * kB * (Te - Tg)
     inelastic = - ne * Ng * np.sum(np.dot(Ae, N) * dE)
     delta = (source + elastic + inelastic) * (2./3) / (kB * ne)
-    #print "source =", source
-    #print "elastic=", elastic
-    #print "inelastic=", inelastic, '\n'
-    #print "delta =", delta
-    #raw_input('')
+    print "source =", source
+    print "elastic=", elastic
+    print "inelastic=", inelastic, '\n'
+    print "delta =", delta
+    raw_input('')
     return delta
 
 # Calculate the equilibrium condition
-ierr = 1.0
-iTOL = 1.0e-6
-n = ne
-while ierr > iTOL:
-    N = solvers.svd(Ae*n + Ao) * Ng
-    ierr = abs(n - N[-1]) / N[-1]
-    n = N[-1]
-    print "N =", N
+#ierr = 1.0
+#iTOL = 1.0e-6
+#n = 1.0
+#while ierr > iTOL:
+#    N = solvers.svd(Ae*n + Ao)
+#    print "N =", N
+#    print "np.sum(N) =", np.sum(N)
+#    raw_input('')
+#    ierr = abs(n - N[-1]) / N[-1]
+#    n = N[-1]
+#N = N * Ng
+#ne = N[-1]
 
 # Initialize solution arrays
 Arad = Ao.clip(min=0)   # Removes depopulation component
 errors = [0.0]
+N = np.array([Ng - ne, 0, 0, 0, 0, 0, 0, ne])
 populations = [N]
 times = [0.0]
 emissions = [np.zeros(Arad.shape)]
 temperatures = [Te]
 
 # Solution loop
-stepper = solvers.rkf45(dNdt, times[0], populations[0], hmax, hmin, TOL)
+#stepper = solvers.rkf45(dNdt, times[0], populations[0], hmax, hmin, TOL)
 start = datetime.now()
 while times[-1] < T:
     ne = N[-1]
-    N, dt, eps = stepper.next()  # Step to next value with generator function
-    #Te = solvers.rk4(dTedt, times[-1], Te, dt) # Advance with same time step
+    N = solvers.rk4(dNdt, times[-1], N, dt)
+    #N, dt, eps = stepper.next()  # Step to next value with generator function
+    Te = solvers.rk4(dTedt, times[-1], Te, dt) # Advance with same time step
     # Using python lists, append is much faster than NumPy equivalent
     emissions.append(Arad * N * dt) #TODO: Verify that this works!
     populations.append(N)
     times.append(times[-1] + dt)
-    errors.append(eps)
+    #errors.append(eps)
     temperatures.append(Te)
 
     # Regenerate temperature-dependent quantities
@@ -102,8 +109,9 @@ while times[-1] < T:
         end = datetime.now()
         print "%g steps" % len(times)
         print "Te =", Te
-        print "Elapsed time:", times[-1]
-        print "Simulation Time:", (end - start), "\n"
+        print "Simulation time:", times[-1], "(dt = %e s)" % (times[-1] -
+                times[-2])
+        print "Elapsed Time:", (end - start), "\n"
 
 # Generate all emission wavelengths in the proper order
 wavelengths = solvers.wavelengths(states, order)

solvers.py
@@ -15,7 +15,7 @@ def svd(matrix):
     """
     u, s, v = np.linalg.svd(matrix)
     #TODO: Is the last row of v always associated with the lowest s?
-    null = np.abs(v[-1,:]).T
+    null = np.abs(v[0,:]).T
     return null
 
 def dE(states, order):
@@ -25,7 +25,7 @@ def dE(states, order):
         Ei = states[order[i]]['E']
         for f in range(dim):
             Ef = states[order[f]]['E']
-            mat[i, f] = Ef - Ei
+            mat[i, f] = (Ef - Ei) / kB
     return mat
 
 def wavelengths(states, order):
@@ -49,7 +49,8 @@ def rk4(f, x, y, h):
     k2 = h * f(x + 0.5*h, y + 0.5*k1)
     k3 = h * f(x + 0.5*h, y + 0.5*k2)
     k4 = h * f(x +     h, y +     k3)
-    return y + k1/6 + k2/3 + k3/3 + k4/6
+    term = y + k1/6 + k2/3 + k3/3 + k4/6
+    return term
 
 def rkf45(f, t0, y0, hmax, hmin, TOL):
     """
@@ -91,8 +92,6 @@ def rkf45(f, t0, y0, hmax, hmin, TOL):
             y = y + (16./135)*k1 + (6656./12825)*k3 + (28561./56430)*k4 \
                   - (9./50)*k5 + (2./55)*k6
             t += h
-            print "y =", y 
-            raw_input('')
             yield y, h, eps
 
         q = 0.84 * (TOL/eps)**0.25