Fixed units in electronic rates for helium.

gases/helium/electronic.py
@@ -2606,7 +2606,7 @@ def rates(Te, istate, fstate):
         rval = np.array([0.0] * len(Tsim))
     interpf = UnivariateSpline(np.array(Tsim), fval, s=0)
     interpr = UnivariateSpline(np.array(Tsim), rval, s=0)
-    return interpf(Te), interpr(Te)
+    return 1e-6 * interpf(Te), 1e-6 * interpr(Te)
 
 def test():
     import matplotlib.pyplot as plt

script.py
@@ -48,14 +48,16 @@ def dTedt(t, Te):
         E = 0
     source = q**2 * ne * E**2 / (me * km * Ng)
     elastic = - ne * km * Ng * (2 * me / M) * 1.5 * kB * (Te - Tg)
-    inelastic = - np.sum(np.dot(ne * Ae * dE, N))
-    delta = (source + elastic + inelastic) * (2./3) / (kB * ne)
-    print "source =", source
-    print "elastic=", elastic
-    print "inelastic=", inelastic
-    print "delta =", delta, '\n'
+    print "N =", N
+    print "Ae =", Ae
+    print "dE =", dE
     raw_input('')
-    return delta
+    inelastic = - np.sum(np.dot(ne * Ae * dE, N))
+    print "source =", dt * (2./3) * source / (kB * ne), "(K)"
+    print "elastic=", dt * (2./3) * elastic / (kB * ne), "(K)"
+    print "inelastic=", dt * (2./3) * inelastic / (kB * ne), "(K)"
+    raw_input('')
+    return (source + elastic + inelastic) * (2./3) / (kB * ne)
 
 # Calculate the equilibrium condition
 if equalize:
@@ -96,7 +98,7 @@ while times[-1] < T:
 
     # Regenerate temperature-dependent quantities
     Ae = matrixgen.electronic(gas, Te)
-    km = matrixgen.km(gas, Te) * Ng/2.688e25    # Generate momentum transfer
+    km = matrixgen.km(gas, Te)/2.688e25    # Generate momentum transfer
 
 
     # Output some useful information every 1000 steps