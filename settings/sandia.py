c329dabc09d378bb056fdf0c4976b9f213756758
Ben Yee <l3enny@gmail.com>
2013-04-10 17:39:42 -0400

    Minor changes to run settings.

--- settings/sandia.py
+++ settings/sandia.py
@@ -2,7 +2,7 @@ from constants import *
 
 # Unnecessary, but convenient values
 Tg = 300                             # neutral gas temperature, K
-P = 3.0 * 133.322                    # neutral gas pressure, Pa
+P = 4.0 * 133.322                    # neutral gas pressure, Pa
 #EN = 3.0e-15 * 1e-4                  # reduced electric field, V m^2
 
 # Physical system options (user-defined)
@@ -10,18 +10,18 @@ P = 3.0 * 133.322                    # neutral gas pressure, Pa
 Te = 0.2 * q / kB                    # effective electron temperature, K
 Ng = P/(kB*Tg)                       # neutral gas density, 1/m^3
 #E0 = EN * Ng                         # The applied electric field
-E0 = 2.25e3 / 1e-1
+E0 = 3.00e3 / 1e-1
 M = 4.002602 * amu                   # Mass of the neutral particle
 
 # WARNING: This value is worthless for the current equilibrium solver
-ne = 1e08 * 1e6                       # background electron density, 1/m^3
+ne = 5.363255e13                      # background electron density, 1/m^3
 
 # Solver options (user-defined)
 T = 1.0e-7          # duration to simulate, s
 #hmin = 1e-18        # minimum time step, s
 #hmax = 1e-09        # maximum time step, s
 dt = 1e-10
-tau = 5e-09
+tau = 1e-08
 #TOL = 1.0e-06       # absolute allowable truncation error, 1/m^3
 
 # Output options (user-defined)
--- visualize.py
+++ visualize.py
@@ -35,7 +35,7 @@ def states(times, states, labels, step=1):
     plt.xlabel('Time ($\mu$s)')
     plt.ylabel('Density (1/m$^3$)')
     plt.legend(l)
-    plt.savefig("populations.png")
+    plt.savefig("populations.pdf")
     plt.hold(False)
     plt.clf()
 
