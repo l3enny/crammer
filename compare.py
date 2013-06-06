d1b63d528c1c729afb2d432fcd920358b15fafac
Ben Yee <l3enny@gmail.com>
2013-06-06 16:20:35 -0400

    Added simple comparison script.

---
+++ compare.py
@@ -0,0 +1,46 @@
+import csv
+import os
+
+from constants import *
+from numpy import loadtxt, linspace, average
+from matplotlib import pyplot as plt
+
+if __name__ == "__main__":
+    pre = '4torr'
+    triplet_metastable = ['210']
+    select = triplet_metastable
+    length = 3.3 * 1e-2 # pathlength of integrated data
+    step = 1        # reduce data density by this amount
+    delay = 282     # negative shift for measured data in ns
+    initial = 0.0   # baseline shift for calculated data
+    zero_correct = True # correct for small offset in measurements
+
+    indices = []
+    with open(pre + '_order.csv') as csvfile:
+        line = csv.reader(csvfile, delimiter=',').next()
+        for s in select:
+            indices.append(line.index(s))
+
+    # Read in data
+    N = []
+    t = []
+    N = loadtxt(pre + '_populations.csv', delimiter=',')
+    t = loadtxt(pre + '_times.csv', delimiter=',')
+    Navg_meas = loadtxt('fitparams.csv', delimiter=',')[:, 1]
+    N_meas = Navg_meas/length
+    t_meas = linspace(0, 2e3, 5e3) * 1e-9
+    if zero_correct:
+        shift = average(N_meas[:200])
+        N_meas = N_meas - shift
+
+    labels = ['Measured', 'Simulated']
+    plt.plot((1e9 * t_meas), N_meas)
+    plt.hold(True)
+    for j in indices:
+        plt.plot(1e9 * t[::step] + delay, N[::step, j] + initial)
+    plt.axis((286, 456, 0, 2e17))
+    plt.xlabel('Time (ns)')
+    plt.ylabel('Density (1/m$^3$)')
+    plt.legend(labels , loc=4)
+    #plt.savefig("compare.pdf")
+    plt.show()
--- settings/s4torr.py
+++ settings/s4torr.py
@@ -5,12 +5,12 @@ from gases import helium as gas      # choose gas to simulate
 import cPickle
 import solvers
 
-with open('./gases/helium/pack_1p0.pickle', mode='r') as f:
+with open('./gases/helium/pack_2p0.pickle', mode='r') as f:
     pack = cPickle.load(f)
 
 km = pack.km
 
-with open('./gases/helium/ralchenko_1p0.pickle', mode='r') as f:
+with open('./gases/helium/ralchenko_2p0.pickle', mode='r') as f:
     coeffs = cPickle.load(f)
 
 solver = solvers.rk4
@@ -32,8 +32,8 @@ M = 4.002604 * amu                   # mass of the neutral particle
 ne = 5.363255e13                     # initial electron density, 1/m^3
 
 # Applied electric field function
-E0 = 1.33140e2 / 1e-2  # amplitude
-tau = 2.0e-8            # width
+E0 = 2.55231e2 / 1e-2   # amplitude
+tau = 5.0e-8            # width
 tail = 0.125            # tail fraction
 t0 = 4.0e-8             # center
 
